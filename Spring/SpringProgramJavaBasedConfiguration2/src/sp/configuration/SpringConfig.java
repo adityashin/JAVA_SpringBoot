345c6fe3de9a252dc6d4dc619f6b40017a579386
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2025-03-13 19:50:59 +0530

    Update SpringConfig.java

--- Spring/SpringProgramJavaBasedConfiguration2/src/sp/configuration/SpringConfig.java
+++ Spring/SpringProgramJavaBasedConfiguration2/src/sp/configuration/SpringConfig.java
@@ -1,5 +1,21 @@
 package sp.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import sp.beans.Student;
+
+@Configuration
 public class SpringConfig {
 
+	@Bean("StudentBeanOBJ")
+	public Student stdid() {
+		
+		Student std = new Student();
+		std.setName("Aditya");
+		std.setRollno(99);
+		std.setEmail("adityashinde@gmail.com");
+		
+		return std;
+	}
 }
