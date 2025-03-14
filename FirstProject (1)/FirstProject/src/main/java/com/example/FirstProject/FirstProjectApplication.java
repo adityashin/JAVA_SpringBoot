e22cd4064f53580560f8288b7c2225a7352fc2f5
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2025-03-14 23:08:48 +0530

    Update FirstProjectApplication.java

--- FirstProject (1)/FirstProject/src/main/java/com/example/FirstProject/FirstProjectApplication.java
+++ FirstProject (1)/FirstProject/src/main/java/com/example/FirstProject/FirstProjectApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstProjectApplication.class, args);
+	public static void main(String[] args) {	SpringApplication.run(FirstProjectApplication.class, args);
 	}
 
 }
