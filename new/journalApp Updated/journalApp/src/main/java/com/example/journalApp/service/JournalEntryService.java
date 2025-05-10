7d0f991bb5a8a9b862f2234a175315c72e8c0772
Aditya Anand Shinde🙋‍♂️ <71961975+adityashin@users.noreply.github.com>
2025-05-10 22:15:55 +0530

    Update JournalEntryService.java

--- new/journalApp Updated/journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
+++ new/journalApp Updated/journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
@@ -1,9 +1,15 @@
 package com.example.journalApp.service;
 
 import com.example.journalApp.Entity.JournalEntry;
+import com.example.journalApp.Entity.User;
 import com.example.journalApp.Repository.JournalEntryRepo;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class JournalEntryService {
@@ -11,7 +17,35 @@ public class JournalEntryService {
     @Autowired
     private JournalEntryRepo journalEntryRepo;
 
+    @Autowired UserEntryService userEntryService;
+
+    public List<JournalEntry> getAll(){
+
+        return journalEntryRepo.findAll();
+    }
+
+    @Transactional
+    public void saveEntry(JournalEntry journalEntry, String username){
+
+        User user = userEntryService.findByUserName(username);
+        JournalEntry saved =  journalEntryRepo.save(journalEntry);
+        user.getJournalEntries().add(saved);
+        userEntryService.saveUser(user);
+    }
+
     public void saveEntry(JournalEntry journalEntry){
         journalEntryRepo.save(journalEntry);
+    }
+
+    public Optional<JournalEntry> getJournalByID(ObjectId mid){
+
+        return journalEntryRepo.findById(mid);
+    }
+
+    public void deletebyId(ObjectId mid,String username){
+        User user = userEntryService.findByUserName(username);
+        user.getJournalEntries().removeIf(x -> x.getId().equals(mid));
+        userEntryService.saveUser(user);
+        journalEntryRepo.deleteById(mid);
     }
 }
