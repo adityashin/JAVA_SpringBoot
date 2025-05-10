b2da8a8c439f76a3014c94d7ed4c639e2b1ac195
Aditya Anand Shinde🙋‍♂️ <71961975+adityashin@users.noreply.github.com>
2025-05-10 22:12:34 +0530

    Update JournalEntry.java

--- new/journalApp Updated/journalApp/src/main/java/com/example/journalApp/Entity/JournalEntry.java
+++ new/journalApp Updated/journalApp/src/main/java/com/example/journalApp/Entity/JournalEntry.java
@@ -1,49 +1,25 @@
 package com.example.journalApp.Entity;
 
+import lombok.*;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "Journal_Entries")
-public class JournalEntry {
+@Data
+@NoArgsConstructor
+public class  JournalEntry {
 
     @Id
-    private String id;
+    private ObjectId id;
 
+    @NonNull
     private String title;
     private String content;
-    private Date date;
+    private LocalDateTime date;
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
